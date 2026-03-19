1bc5a0d01778334177ddda50a9ed036684941ae6
Bilal <bilal@example.com>
2026-03-19 23:23:24 +0000

    Add sitemap and robots for indexing

---
+++ src/app/sitemap.ts
@@ -0,0 +1,46 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://contractforge.co.uk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/client-onboarding-pack",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/personal-trainer-contract-template-uk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/par-q-form-template-uk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/liability-waiver-template-uk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/personal-trainer-cancellation-policy-template",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/personal-trainer-payment-terms-template",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/terms",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://contractforge.co.uk/privacy",
+      lastModified: new Date(),
+    },
+  ];
+}
